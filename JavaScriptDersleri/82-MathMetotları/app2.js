3b307187d1758b2aa41fee414555a04308b622e8
Semih AÇMALI <suhedasemihacmali@gmail.com>
2024-05-27 10:31:52 +0300

    Add files via upload

---
+++ JavaScriptDersleri/82-MathMetotları/app2.js
@@ -0,0 +1,40 @@
+let sayi = 79.05;
+console.log(Math.floor(sayi));
+console.log(Math.ceil(sayi));
+console.log(Math.round(sayi));
+let sayidizi=[75,89,53,47,33,86,54,2,64];
+
+/*
+let enkucuk = sayidizi[0];
+for(let i=1;i<sayidizi.length;i++){
+    if(enkucuk >sayidizi[i]){
+        enkucuk=sayidizi [i];
+        console.log (enkucuk);
+    }
+}
+console.log (enkucuk);
+*/
+
+
+/*
+for(let i=0;i<sayidizi.length;i++){
+    for(let j=i+1; j<sayidizi.length; j++){
+        if (sayidizi[i]>sayidizi[j]){
+            let gecici= sayidizi [i];
+            sayidizi [i] = sayidizi[j];
+            sayidizi [j] = gecici
+        }  
+    }
+    console. log (sayidizi);
+}
+console. log (sayidizi);
+*/
+
+console.log(Math.sqrt(29));
+console.log(Math.pow(5,2));
+console.log(Math.pow(Math.round(Math.pow(125,(1/3))),3));
+
+
+console.log(Math.abs(-15));
+
+console.log(Math.PI);
